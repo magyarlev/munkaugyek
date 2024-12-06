2374c09f0ae42fc03d944c72acd3913a01b8d7d8
MagyarL <magyar.levi@gmail.com>
2024-12-06 13:43:38 +0100

    add get cases, reister, delete user endpoints

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import Cases from "../modules/cases";
-import Users from "../modules/user";
+import User from "../modules/user";
+import { emitWarning } from "process";
+import user from "../modules/user";
 
 const router = express.Router();
 const db = "mongodb://localhost:27017/munkaügyek";
@@ -17,11 +19,35 @@ mongoose
 
 router.get("/cases", async (req: Request, res: Response) => {
   try {
-    let cases = await Users.find({});
+    let cases = await Cases.find({});
     res.status(200).json(cases);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal server error.");
+  }
+});
+
+router.post("/user", async (req: Request, res: Response) => {
+  try {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const registeredUser = await newUser.save();
+    res.status(200).send("User created successfully").json(registeredUser);
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+router.delete("/user", async (req: Request, res: Response) => {
+  try {
+    const userId = req.body._id;
+    await user.findById(userId).deleteOne();
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    res.status(500).send("Internal server error");
   }
 });
 
