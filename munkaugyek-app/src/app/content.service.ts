1861008d3ef0edb69a044710617365ebad71154a
MagyarL <magyar.levi@gmail.com>
2024-12-17 22:56:41 +0100

    add backend method declarations

--- munkaugyek-app/src/app/content.service.ts
+++ munkaugyek-app/src/app/content.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ContentService {
+  getAllItems() {}
+  getItemById() {}
+  addNewItem() {}
+  deleteItem() {}
+  updateItem() {}
+
   #items = [
     {
       name: 'test1',
