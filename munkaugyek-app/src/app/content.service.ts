3a10aaf737981eb42cbde73b3fd496bebcf5d308
MagyarL <magyar.levi@gmail.com>
2025-01-03 14:45:17 +0100

    update content service

--- munkaugyek-app/src/app/content.service.ts
+++ munkaugyek-app/src/app/content.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContentService {
+  httpClient = inject(HttpClient);
   getAllItems() {}
   getItemById() {}
+
   addNewItem() {}
+
   deleteItem() {}
   updateItem() {}
 
